0838a9acf82407a2ee06085504b7b93519ce278d
mohammad <mohammadnasimi6803@gmail.com>
2021-12-31 00:38:38 +0330

    class food

---
+++ class_oop.py
@@ -0,0 +1,39 @@
+class food:
+    def __init__(self, id, name, category, price, count):
+        self.id = id
+        self.name = name
+        self.category = category
+        self.price = price
+        self.count = count
+
+    @classmethod
+    def show_food(cls):
+        pass
+
+    @classmethod
+    def food_price(cls):
+        pass
+
+    @classmethod
+    def add_food(cls):
+        pass
+
+    @classmethod
+    def change_food(cls):
+        pass
+
+    @classmethod
+    def remove_food(cls):
+        pass
+
+    @classmethod
+    def change_count_food(cls):
+        pass
+
+    @classmethod
+    def price_food(cls):
+        pass
+
+    @classmethod
+    def is_available_food(cls):
+        pass
