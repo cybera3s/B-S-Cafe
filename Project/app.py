abb7bc3c67176b968bbe6a79faf5233a3a360d1d
Alireza <alirezac257@gmail.com>
2022-01-03 14:20:43 +0330

    Phase#1 Routes added (home,order,menu)

--- Project/app.py
+++ Project/app.py
@@ -3,6 +3,10 @@ from views import *
 
 app = Flask(__name__)
 
+app.add_url_rule('/', 'index', index, methods=['GET'])
+app.add_url_rule('/menu', 'menu', menu, methods=['GET'])
+app.add_url_rule('/order/<table_id>', 'index', order, methods=['GET', 'POST', 'DELETE'])
+
 
 if __name__ == '__main__':
     app.run()
