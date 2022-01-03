6a07e4ac1660cfd0990f73ce07f3dc8d03e7e667
Alireza <alirezac257@gmail.com>
2022-01-04 01:15:23 +0330

    contact_us and about_us routes added

--- Cafe_project/app.py
+++ Cafe_project/app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 app.add_url_rule('/', 'home', index, methods=['GET'])
 app.add_url_rule('/menu', 'menu', menu, methods=['GET'])
+app.add_url_rule('/about_us', 'about_us', menu, methods=['GET'])
+app.add_url_rule('/contact_us', 'contact_us', menu, methods=['GET', 'POST'])
 app.add_url_rule('/order/<table_id>', 'order', order, methods=['GET', 'POST', 'DELETE'])
 
 
