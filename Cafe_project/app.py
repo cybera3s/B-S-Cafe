4bada147f098eed30dfd80a97829292deb97f89e
mohammad <mohammadnasimi6803@gmail.com>
2022-01-03 15:52:23 +0330

    refactor app.py

--- Cafe_project/app.py
+++ Cafe_project/app.py
@@ -3,12 +3,10 @@ from views import *
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 app.add_url_rule('/', 'index', index, methods=['GET'])
 app.add_url_rule('/menu', 'menu', menu, methods=['GET'])
 app.add_url_rule('/order/<table_id>', 'order', order, methods=['GET', 'POST', 'DELETE'])
-=======
->>>>>>> feature_safa
+
 
 if __name__ == '__main__':
     app.run()
