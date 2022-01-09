5e078de2b83a54e2b03e41b2b94b28ca5e0073fb
Alireza <alirezac257@gmail.com>
2022-01-09 19:02:15 +0330

    Template folder removed

--- Cafe_project/app.py
+++ Cafe_project/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from views.landing_views import *
 from views import *
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 #  -----------  Landing Pages----------------------
 
