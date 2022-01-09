ea63422ed4885ccfac7a79b08e4517513d49b6af
Alireza <alirezac257@gmail.com>
2022-01-10 01:59:42 +0330

    base variables refactor and home func added

--- Cafe_project/views/landing_views.py
+++ Cafe_project/views/landing_views.py
@@ -1,29 +1,51 @@
 import flask
 from flask import url_for, request, redirect, render_template
+import json
 from database.manager import db
 from models import models
 
 base_variables = {
-    "page": {
-        "lang": 'en-US',
-        "title": ''
+    "pages": {
+        "home": {
+            'title': 'خانه',
+            'endpoint': 'home'
+        },
+        "menu": {
+            'title': 'منو',
+            'endpoint': 'menu'
+        },
+        "about_us": {
+            'title': 'درباره ما',
+            'endpoint': 'about_us'
+        },
+        "contact_us": {
+            'title': 'ارتباط با ما',
+            'endpoint': 'contact_us'
+        },
     },
-    "links": ["home", "menu", "about_us", "contact_us"]
+    "current_page": ''
 }
 
 
 def index():
     data = base_variables
+    data['current_page'] = 'index'
     tables = db.read_all(models.Table)
-    data['page']['title'] = 'home'
     if request.method == 'GET':
         data["title"] = 'home'
         return render_template("index.html", data=data, tables=tables)
 
 
+def home():
+    data = base_variables
+    data['current_page'] = 'index'
+    if request.method == 'GET':
+        return render_template("home.html", data=data)
+
+
 def menu():
     data = base_variables
-    data["page"]["title"] = "menu"
+    data['current_page'] = 'menu'
     items = db.read_all(models.MenuItems)
     if request.method == 'GET':
         data["title"] = 'menu'
@@ -32,28 +54,30 @@ def menu():
 
 def order(table_id):
     data = base_variables
+    data['current_page'] = 'order'
     items = db.read_all(models.MenuItems)
     table = db.read(models.Table, table_id)
-    data['page']["title"] = 'order'
     if request.method == 'GET':
         res = flask.make_response(render_template('order.html', data=data, items=items))
         res.set_cookie('table_id', table_id)
+
+        res.set_cookie('orders', json.dumps())
         return res
     elif request.method == 'POST':
-        return f'POST/Order Page !{table_id} '
+        return f'POST/Order Page !{table_id}'
     elif request.method == 'DELETE':
         return f'DELETE/Order Page !{table_id}'
 
 
 def about_us():
     data = base_variables
-    data['page']['title'] = 'About Us'
+    data['current_page'] = 'about_us'
     if request.method == 'GET':
         return render_template('about_us.html', data=data)
 
 
 def contact_us():
     data = base_variables
-    data['page']['title'] = 'About Us'
+    data['current_page'] = 'contact_us'
     if request.method == 'GET':
         return render_template('contact_us.html', data=data)
