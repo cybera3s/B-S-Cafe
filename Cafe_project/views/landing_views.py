4b25365aba40e71298b25df11147c1c42e65ae32
Alireza <alirezac257@gmail.com>
2022-01-09 18:41:12 +0330

    table_id Cookie set

--- Cafe_project/views/landing_views.py
+++ Cafe_project/views/landing_views.py
@@ -1,3 +1,4 @@
+import flask
 from flask import url_for, request, redirect, render_template
 from database.manager import db
 from models import models
@@ -17,7 +18,6 @@ def index():
     data['page']['title'] = 'home'
     if request.method == 'GET':
         data["title"] = 'home'
-        # return "<h1>Test Runing...</h1>"
         return render_template("index.html", data=data, tables=tables)
 
 
@@ -36,7 +36,9 @@ def order(table_id):
     table = db.read(models.Table, table_id)
     data['page']["title"] = 'order'
     if request.method == 'GET':
-        return render_template('order.html', data=data, items=items)
+        res = flask.make_response(render_template('order.html', data=data, items=items))
+        res.set_cookie('table_id', table_id)
+        return res
     elif request.method == 'POST':
         return f'POST/Order Page !{table_id} '
     elif request.method == 'DELETE':
