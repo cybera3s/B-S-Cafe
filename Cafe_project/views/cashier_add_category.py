d244a7194254d7c35fa6a924ea0fb58ba93d4dea
mohammad <mohammadnasimi6803@gmail.com>
2022-01-09 21:41:52 +0330

    cashier_add_category  def in py

---
+++ Cafe_project/views/cashier_add_category.py
@@ -0,0 +1,26 @@
+from flask import url_for, request, redirect, render_template, make_response
+
+from database import manager
+from database.manager import db
+from models import models
+from models.models import Order
+from views.landing_views import base_variables
+
+
+# Cashier
+def cashier_add_category():
+    data = base_variables
+    data["page"]["title"] = "category"
+    items = db.read_all(models.Category)
+    if request.method == 'GET':
+        data["title"] = 'category'
+        return render_template('cashier/cashier_add_category.html')
+    elif request.method == "POST":
+        name = request.form.get('name')
+        root = request.form.get('root')
+        discount = request.form.get('discount')
+        print(name, root, discount)
+        category = models.Category(name, root)
+        db.create(category)
+        resp = make_response(redirect(url_for('cashier_add_category')))
+        return resp
