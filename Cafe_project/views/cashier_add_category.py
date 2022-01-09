6ae02e376f368c0e2c01776d633cfe4069677aea
mohammad <mohammadnasimi6803@gmail.com>
2022-01-09 23:03:57 +0330

    cashier_add_category/views/jinja/database

--- Cafe_project/views/cashier_add_category.py
+++ Cafe_project/views/cashier_add_category.py
@@ -11,16 +11,18 @@ from views.landing_views import base_variables
 def cashier_add_category():
     data = base_variables
     data["page"]["title"] = "category"
-    items = db.read_all(models.Category)
+    items_category = db.read_all(models.Category)
+    items_discount = db.read_all(models.Category)
     if request.method == 'GET':
         data["title"] = 'category'
-        return render_template('cashier/cashier_add_category.html')
+        return render_template('cashier/cashier_add_category.html', items_category=items_category, data=data,
+                               items_discount=items_discount)
     elif request.method == "POST":
         name = request.form.get('name')
         root = request.form.get('root')
         discount = request.form.get('discount')
         print(name, root, discount)
-        category = models.Category(name, root)
-        db.create(category)
+        # category = models.Category(name, root)
+        # db.create(category)
         resp = make_response(redirect(url_for('cashier_add_category')))
         return resp
