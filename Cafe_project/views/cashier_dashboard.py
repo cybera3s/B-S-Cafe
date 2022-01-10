cb4f96ed50d09b64062e13cc028ac801745b8183
ario <cybera.3s@gmail.com>
2022-01-10 21:58:49 +0330

    add last week report to view

--- Cafe_project/views/cashier_dashboard.py
+++ Cafe_project/views/cashier_dashboard.py
@@ -1,12 +1,20 @@
 from flask import url_for, request, redirect, render_template, make_response, flash
 from database.manager import db
 from models import models
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def cashier_dashboard():
+
     all_receipts = db.read_all(models.Receipt)
 
     today_receipts = list(filter(lambda order: order.create_time.day == datetime.now().day, all_receipts))
-    today_earnings = list(map(lambda order))
-    return render_template('cashier/dashboard.html', customer_count=len(today_receipts))
+    today_earnings = sum(list(map(lambda order: order.final_price, today_receipts)))
+
+    report = models.Receipt.last_week_report(all_receipts=all_receipts)
+
+    labels = list(map(lambda i: i[1], report))
+    sum_receipts = list(map(lambda i: i[0], report))
+
+
+    return render_template('cashier/dashboard.html', customer_count=len(today_receipts), today_earnings=today_earnings)
