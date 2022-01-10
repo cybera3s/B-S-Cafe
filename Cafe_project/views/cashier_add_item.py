8de4352ed7f07c806681d375ae4ff1d5363530f4
mohammadrezaho <mohammadrezasm779@gmail.com>
2022-01-10 23:21:18 +0330

    bug fix

--- Cafe_project/views/cashier_add_item.py
+++ Cafe_project/views/cashier_add_item.py
@@ -27,8 +27,11 @@ def cashier_add_item():
         price = request.form['price']
         serving_time_period = request.form['serving']
         estimated_cooking_time = request.form['estimated']
-        discount_id = request.form['discount']
-        category_id = request.form['category']
+        discount_value = int(request.form['discount'])
+        print(discount_value)
+        discount_id = list(filter(lambda d: d.value == discount_value, discount))[0].id
+        category_name = request.form['category']
+        category_id = list(filter(lambda c: c.category == category_name, category))[0].id
         new_item = MenuItems(name=name, price=price, category_id=category_id, serving_time_period=serving_time_period,
                              estimated_cooking_time=estimated_cooking_time, picture_link=image_url,
                              discount_id=discount_id)
