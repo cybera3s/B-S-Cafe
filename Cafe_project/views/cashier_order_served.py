ee4c9afe70626dd1ca17820587c476e87f9225d4
mohammad <mohammadnasimi6803@gmail.com>
2022-01-10 23:36:07 +0330

    cashier_add_served add name menu_items in order instead id

--- Cafe_project/views/cashier_order_served.py
+++ Cafe_project/views/cashier_order_served.py
@@ -1,7 +1,9 @@
+from datetime import date
+
 from flask import url_for, request, redirect, render_template
 from database.manager import db
 from models import models
-from models.models import Order
+from models.models import Order, MenuItems, Receipt
 from views.landing_views import base_variables
 
 
@@ -10,4 +12,7 @@ def cashier_order_served():
     data = base_variables
     data["page"]["title"] = "served"
     items = db.read_by(Order, ('status_id', 1))
+    for i in items:
+        x = db.read(MenuItems, i.menu_item)
+        i.menu_item = x.name
     return render_template('cashier/Cashier_order_served.html', items=items, data=data)
