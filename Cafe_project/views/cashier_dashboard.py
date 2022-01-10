289adbe00c6d68b324cc6ba899b9b621828f8339
ario <cybera.3s@gmail.com>
2022-01-10 20:10:48 +0330

    add today_earnings

--- Cafe_project/views/cashier_dashboard.py
+++ Cafe_project/views/cashier_dashboard.py
@@ -1,11 +1,12 @@
 from flask import url_for, request, redirect, render_template, make_response, flash
 from database.manager import db
 from models import models
+from datetime import datetime
 
 
 def cashier_dashboard():
-    today_customer_count = db.read_all(models.Receipt)
-    print(today_customer_count)
-    print(today_customer_count.sort(key=lambda o: o.create_time))
+    all_receipts = db.read_all(models.Receipt)
 
-    return render_template('cashier/dashboard.html', customer_count=len(today_customer_count))
+    today_receipts = list(filter(lambda order: order.create_time.day == datetime.now().day, all_receipts))
+    today_earnings = list(map(lambda order))
+    return render_template('cashier/dashboard.html', customer_count=len(today_receipts))
