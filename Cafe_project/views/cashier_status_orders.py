aafc798f4979631a92d3dd2769e7a7ce78c126b7
mohammad <mohammadnasimi6803@gmail.com>
2022-01-11 15:35:39 +0330

    cashier order served send title_get for page

--- Cafe_project/views/cashier_status_orders.py
+++ Cafe_project/views/cashier_status_orders.py
@@ -10,42 +10,46 @@ from views.landing_views import base_variables
 def cashier_new_order():
     data = base_variables
     data["page"]["title"] = "New Orders"
+    title_get = 'cashier_new_order'
     items = db.read_by(Order, ('status_id', 1))
     for i in items:
         x = db.read(MenuItems, i.menu_item)
         i.menu_item = x.name
         i.create_time = i.create_time.strftime("%Y/%-m/%d  %-I:%m ")
-    return render_template('cashier/Cashier_order_served.html', items=items, data=data)
+    return render_template('cashier/Cashier_order_served.html', items=items, data=data, title_get=title_get)
 
 
 def cashier_cook_order():
     data = base_variables
     data["page"]["title"] = "Cooking Orders"
+    title_get = 'cashier_cook_order'
     items = db.read_by(Order, ('status_id', 2))
     for i in items:
         x = db.read(MenuItems, i.menu_item)
         i.menu_item = x.name
         i.create_time = i.create_time.strftime("%Y/%-m/%d  %-I:%m ")
-    return render_template('cashier/Cashier_order_served.html', items=items, data=data)
+    return render_template('cashier/Cashier_order_served.html', items=items, data=data, title_get=title_get)
 
 
 def cashier_delete_order():
     data = base_variables
     data["page"]["title"] = "Delete Orders"
+    title_get = 'cashier_delete_order'
     items = db.read_by(Order, ('status_id', 4))
     for i in items:
         x = db.read(MenuItems, i.menu_item)
         i.menu_item = x.name
         i.create_time = i.create_time.strftime("%Y/%-m/%d  %-I:%m ")
-    return render_template('cashier/Cashier_order_served.html', items=items, data=data)
+    return render_template('cashier/Cashier_order_served.html', items=items, data=data, title_get=title_get)
 
 
 def cashier_paid_order():
     data = base_variables
     data["page"]["title"] = "Paid Orders"
+    title_get = 'cashier_paid_order'
     items = db.read_by(Order, ('status_id', 5))
     for i in items:
         x = db.read(MenuItems, i.menu_item)
         i.menu_item = x.name
         i.create_time = i.create_time.strftime("%Y/%-m/%d  %-I:%m ")
-    return render_template('cashier/Cashier_order_served.html', items=items, data=data)
+    return render_template('cashier/Cashier_order_served.html', items=items, data=data, title_get=title_get)
