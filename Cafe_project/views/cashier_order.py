23e6110a0ae7dbd9cccf9efd2f6fb92e3e1f3f10
mohammadrezaho <mohammadrezasm779@gmail.com>
2022-01-11 20:48:44 +0330

    edit cashier_add_item.html

--- Cafe_project/views/cashier_order.py
+++ Cafe_project/views/cashier_order.py
@@ -1,9 +1,10 @@
 from flask import url_for, request, redirect, render_template, flash
 from database.manager import DBManager, db
-from models.models import Order
+from models.models import Order, Receipt
 
 
 def cashier_order():
-    order = db.read_all(Order)
-    print(list(filter(lambda o: o.menu_item, order))[0].menu_item)
-    return render_template('cashier/order.html')
+    orders = db.read_all(Order)
+    receipts = db.read_all(Receipt)
+
+    return render_template('cashier/order.html', orders=orders, receipts=receipts)
