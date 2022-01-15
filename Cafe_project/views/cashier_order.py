2e56d339c6d43bd72f4b4a0c957a1c8c4997e6c7
mohammadrezaho <mohammadrezasm779@gmail.com>
2022-01-15 16:23:45 +0330

    refactor cashier_order.py

--- Cafe_project/views/cashier_order.py
+++ Cafe_project/views/cashier_order.py
@@ -1,10 +1,16 @@
 from flask import url_for, request, redirect, render_template, flash
 from database.manager import DBManager, db
-from models.models import Order, Receipt
+from models.models import Order, Receipt, MenuItems
 
 
 def cashier_order():
-    orders = db.read_all(Order)
-    receipts = db.read_all(Receipt)
+    if request.method == 'GET':
+        receipts = db.read_all(Receipt)
+        return render_template('cashier/order.html', receipts=receipts)
+    if request.method == 'POST':
+        request_data = request.get_json()
+        orders = db.read_by(Order, ('receipt_id', request_data['receipt']))
+        menu_items = db.read_all(MenuItems)
+        return render_template('cashier/receipt-modify.html', orders=orders, items=menu_items)
 
-    return render_template('cashier/order.html', orders=orders, receipts=receipts)
+
