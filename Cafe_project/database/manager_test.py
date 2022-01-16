29c39f5d0427680c6c28daffd6877864d96412e4
mohammad <mohammadnasimi6803@gmail.com>
2022-01-16 22:04:43 +0330

    create database test/read 2 test

--- Cafe_project/database/manager_test.py
+++ Cafe_project/database/manager_test.py
@@ -10,15 +10,29 @@ class DBManagerTest(TestCase):
         self.db_manager = db
 
     def test_create_success1(self):
-        self.P1 = Category('پیتزا پپرونی', 18, 2)
-        res = self.db_manager.create(self.P1)
+        self.C1 = Category('پیتزا پپرونی', 18, 2)
+        res = self.db_manager.create(self.C1)
 
         self.assertIsInstance(res, int)
-        self.assertEqual(self.P1.id, res)
+        self.assertEqual(self.C1.id, res)
 
     def test_create_success2(self):
-        self.P1 = Order(12, 1, 177, 2)
-        res = self.db_manager.create(self.P1)
+        self.C2 = Order(12, 1, 177, 2)
+        res = self.db_manager.create(self.C2)
 
         self.assertIsInstance(res, int)
-        self.assertEqual(self.P1.id, res)
+        self.assertEqual(self.C2.id, res)
+
+    def test_read_success1(self):
+        if not hasattr(self, 'C1'):
+            self.test_create_success1()
+        read = self.db_manager.read(Category, self.C1.id)
+
+        self.assertEqual(vars(read), vars(self.C1))
+
+    def test_read_success2(self):
+        if not hasattr(self, 'C2'):
+            self.test_create_success2()
+        read = self.db_manager.read(Order, self.C2.id)
+
+        self.assertEqual(vars(read), vars(self.C2))
