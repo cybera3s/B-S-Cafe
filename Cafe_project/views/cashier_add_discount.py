23873bdbb4f9dfe590d9725e77e670b98f9023be
Meysam Nosrati <meysamna514@gmail.com>
2022-01-16 21:29:46 +0330

    write views for add discount

--- Cafe_project/views/cashier_add_discount.py
+++ Cafe_project/views/cashier_add_discount.py
@@ -1,6 +1,16 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, flash
+from database.manager import db
+from models.models import Discount
 
 
-def test():
+def cashier_new_discount():
     if request.method == "GET":
-        return render_template("cashier/baseCashier.html")
+        return render_template("cashier/cashier_add_discount.html")
+    elif request.method == "POST":
+        disValue = request.form['persentDiscount']
+
+        newDiscount = Discount(value=int(disValue))
+
+        db.create(newDiscount)
+        flash("Discount successfuly added!")
+        return redirect(request.url)
