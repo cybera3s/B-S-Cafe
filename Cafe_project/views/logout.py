9186930d9759d9dba00b5e337a69bb8e4747a592
ario <cybera.3s@gmail.com>
2022-01-18 00:42:44 +0330

    refactor del cookie

--- Cafe_project/views/logout.py
+++ Cafe_project/views/logout.py
@@ -4,6 +4,5 @@ from flask import url_for, redirect, make_response, request
 def logout():
     """ log out"""
     resp = make_response(redirect(url_for('login')))
-    resp.delete_cookie('user', path='/', domain='127.0.0.1')
-    print(1)
+    resp.delete_cookie('user')
     return resp
