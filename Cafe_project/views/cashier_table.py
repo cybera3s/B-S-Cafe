bd7494078a841685a57d0163d4a2a15a3985d817
ario <cybera.3s@gmail.com>
2022-01-17 16:35:08 +0330

    handle AJAX POST request

--- Cafe_project/views/cashier_table.py
+++ Cafe_project/views/cashier_table.py
@@ -19,4 +19,13 @@ def cashier_table():
         }
     }
 
+    # handle AJAX POST request for changing state of tables
+    if request.method == "POST":
+        data = request.get_json()
+        id = data['id']
+        table = db.read(Table, id)
+        table.status = False
+        db.update(table)
+        return '200'
+
     return render_template('cashier/tables.html', data=data)
