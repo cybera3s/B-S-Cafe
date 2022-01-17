3aae02a0ee5da29b82dafa12bbc2a7c8e42db52d
mohammad <mohammadnasimi6803@gmail.com>
2022-01-17 20:14:58 +0330

    cashier log out views

--- Cafe_project/views/logout.py
+++ Cafe_project/views/logout.py
@@ -1,2 +1,9 @@
+from flask import url_for, redirect, make_response, request
+
+
 def logout():
-    pass
+    """ log out"""
+    resp = make_response(redirect(url_for('login')))
+    resp.delete_cookie('user', path='/', domain='127.0.0.1')
+    print(1)
+    return resp
