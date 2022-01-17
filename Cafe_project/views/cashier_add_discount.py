5f8ef4ea9598ab16e5f032f88804b72fcdbccf32
ario <cybera.3s@gmail.com>
2022-01-18 00:45:48 +0330

    route protecting

--- Cafe_project/views/cashier_add_discount.py
+++ Cafe_project/views/cashier_add_discount.py
@@ -1,9 +1,13 @@
-from flask import render_template, request, redirect, flash
+from flask import render_template, request, redirect, flash, url_for
 from database.manager import db
 from models.models import Discount
 
 
 def cashier_new_discount():
+    # route protecting
+    user_email = request.cookies.get('user')
+    if not user_email:
+        return redirect(url_for('login'))
     if request.method == "GET":
         return render_template("cashier/cashier_add_discount.html")
     elif request.method == "POST":
