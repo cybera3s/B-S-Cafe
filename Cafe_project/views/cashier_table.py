95d097a63574b0f11008dd59b6318bd026005545
ario <cybera.3s@gmail.com>
2022-01-17 16:33:32 +0330

    read tables and change status

--- Cafe_project/views/cashier_table.py
+++ Cafe_project/views/cashier_table.py
@@ -2,7 +2,15 @@ from flask import render_template, request
 from database.manager import db
 from models.models import Table
 
+
 def cashier_table():
+    tables = db.read_all(Table)  # fetch all of tables from database
+
+    # turn status of boolean to string
+    for table in tables:
+        table.status = 'Busy' if table.status else 'Free'
+
+
     data = {
         "page": {
             "title": "tables",
