1415c8fab79d5ff43e991cbac39cd9ced0235cbc
ario <cybera.3s@gmail.com>
2022-01-18 13:22:57 +0330

    add curren_user and data

--- Cafe_project/views/cashier_add_discount.py
+++ Cafe_project/views/cashier_add_discount.py
@@ -1,15 +1,20 @@
 from flask import render_template, request, redirect, flash, url_for
 from database.manager import db
 from models.models import Discount
+from views.get_current_user import get_current_user
 
 
 def cashier_new_discount():
     # route protecting
-    user_email = request.cookies.get('user')
-    if not user_email:
+    user = get_current_user()
+    if not user:
         return redirect(url_for('login'))
+    data = {
+        'user': user,
+
+    }
     if request.method == "GET":
-        return render_template("cashier/cashier_add_discount.html")
+        return render_template("cashier/cashier_add_discount.html", data=data)
     elif request.method == "POST":
         disValue = request.form['persentDiscount']
         status = None
@@ -18,7 +23,7 @@ def cashier_new_discount():
             db.create(newDiscount)
             msg = "discount successfully added!"
             status = "success"
-        except Exception as e :
+        except Exception as e:
             msg = "The entered value is Duplicated!"
             status = "danger"
         flash(msg, status)
