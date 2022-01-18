389c9ad748fc79b6873988e51a4c5d2eb35e9326
ario <cybera.3s@gmail.com>
2022-01-18 13:45:19 +0330

    remove unused import

--- Cafe_project/views/cashier_status_orders.py
+++ Cafe_project/views/cashier_status_orders.py
@@ -1,4 +1,4 @@
-from flask import url_for, request, redirect, render_template
+from flask import url_for, redirect, render_template
 from database.manager import db
 from models.models import Order, MenuItems
 from views.get_current_user import get_current_user
