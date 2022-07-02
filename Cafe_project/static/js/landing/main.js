2a22669c2264f10b853bab89de3319c873ccf30b
ario <cybera.3s@gmail.com>
2022-07-02 14:27:53 +0430

    refactor

--- Cafe_project/static/js/landing/main.js
+++ Cafe_project/static/js/landing/main.js
@@ -1,20 +1,26 @@
 /*jshint esversion: 6 */
 $( document ).ready(function() {
-    console.log( "Document is ready!" );
 
-    function table_select(table_id) {
-    let target_url = 'http://127.0.0.1:5000/order';
-    $("#page-loader").empty();
-    $.ajax({
-        url: `${target_url}/${table_id}`,
-        type: "GET",
-        success: function (response) {
-            $("#page-loader").append(`${response}`)
-        }
-    })
-}
+    console.log("Document is ready!");
+    const BASE_URL = 'http://127.0.0.1:5000';
 
-//  nav bar link on click event
+    // Table click event
+    $(".table-item").click(function () {
+        $("#page-loader").empty();
+        let tableId = $(this).attr('id');
+        let target_url = 'http://127.0.0.1:5000/order/' + tableId;
+        console.log(`table ${tableId} clicked`);
+
+        $.get(
+            target_url,
+            function (data) {
+                $("#page-loader").append(data);
+                console.log(data.headers);
+            }
+        ).catch(err => console.log(err.responseText));
+    });
+
+    //  nav bar link on click event
     $(".nav-link-spa").click( function(e) {
         e.preventDefault();
         console.log("nav link clicked");
@@ -64,38 +70,42 @@ $( document ).ready(function() {
             }
         );
 
-function cart_loader() {
-    let target_url = "{{ url_for('cart') }}"
-    $.ajax({        // ajax request to send order details to flask
-        url: target_url,
-        type: "GET",
-        success: function (resp) {
-            $('#cart-loader').empty();
-            $('#cart-loader').append(resp);
-        }
-    })
-}
+    });
 
-// function payment() {
-//     $('#page-loader').empty();
-//     let table_id = $.cookie("table_id");
-//     let receipt = $.cookie("receipt_id");
-//     let target_url = '{{ url_for('cart') }}';
-//     let postData = {
-//         receipt: receipt,
-//         table: table_id
-//     }
-//     $.ajax({
-//         url: target_url,
-//         type: 'POST',
-//         contentType: "application/json",
-//         data: JSON.stringify(postData),
-//         success: function (response) {
-//             $('#page-loader').append(response)
-//             swal('payment successful ', `Receipt Number : ${$.cookie('receipt_id')}`, 'success')
-//             $.removeCookie("receipt_id", {path: '/'});
-//         }
-//     })
-// }
+    // show cart modal click event
+    $("#cart-float-btn").click(function () {
+        let target_url = BASE_URL + '/cart';
+
+        $.get(
+            target_url,
+            function (data) {
+                $('#cart-loader').empty();
+                $('#cart-loader').append(data);
+            }
+        );
+    });
+
+    // function payment() {
+    //     $('#page-loader').empty();
+    //     let table_id = $.cookie("table_id");
+    //     let receipt = $.cookie("receipt_id");
+    //     let target_url = '{{ url_for('cart') }}';
+    //     let postData = {
+    //         receipt: receipt,
+    //         table: table_id
+    //     }
+    //     $.ajax({
+    //         url: target_url,
+    //         type: 'POST',
+    //         contentType: "application/json",
+    //         data: JSON.stringify(postData),
+    //         success: function (response) {
+    //             $('#page-loader').append(response)
+    //             swal('payment successful ', `Receipt Number : ${$.cookie('receipt_id')}`, 'success')
+    //             $.removeCookie("receipt_id", {path: '/'});
+    //         }
+    //     })
+    // }
 });
+
 
