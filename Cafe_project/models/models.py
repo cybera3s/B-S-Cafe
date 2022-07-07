2278367ae3a5c42dbf59b273576e97574ab48274
ario <cybera.3s@gmail.com>
2022-07-07 17:19:13 +0430

    refactor init method > Cashier models

--- Cafe_project/models/models.py
+++ Cafe_project/models/models.py
@@ -208,13 +208,13 @@ class Cashier(DBModel):
     PK = "id"
 
     def __init__(
-        self,
-        first_name: str,
-        last_name: str,
-        phone_number: str,
-        email: str,
-        password: str,
-        id: int = None,
+            self,
+            first_name: str,
+            last_name: str,
+            phone_number: str,
+            email: str,
+            password: str,
+            id: int = None,
     ):
         self.first_name = first_name
         self.last_name = last_name
