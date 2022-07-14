0954fb562a664c0ed24e7338be846085a2a8b48c
ario <cybera.3s@gmail.com>
2022-07-14 17:54:42 +0430

    add available_tables view

--- Cafe_project/landing/views.py
+++ Cafe_project/landing/views.py
@@ -16,6 +16,12 @@ base_variables = {
 }
 
 
+def available_tables():
+    tables = db.read_by(models.Table, ("status", "FALSE"))  # read tables that are empty
+    serialized_tables = list(map(lambda t: vars(t), tables))
+    return jsonify(serialized_tables)
+
+
 def index():
     data = base_variables
     data["current_page"] = "index"
