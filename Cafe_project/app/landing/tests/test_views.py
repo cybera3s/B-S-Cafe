54219c2f7420f31cd5d2227276d29feff3e0680f
ario <cybera.3s@gmail.com>
2022-07-21 14:39:14 +0430

    refactor test_get_request_index_should_succeed

--- Cafe_project/app/landing/tests/test_views.py
+++ Cafe_project/app/landing/tests/test_views.py
@@ -1,7 +1,9 @@
 from .conftest import *
+from flask import url_for
 
 
-def test_get_request_root_should_succeed(client):
-    response = client.get("/")
-    assert "<title>Home</title>" in response.get_data(as_text=True)
+def test_get_request_index_should_succeed(client):
+    url = url_for('landing.index')
+    response = client.get(url)
+    assert b"<title>Home</title>" in response.data
     assert response.status_code == 200
