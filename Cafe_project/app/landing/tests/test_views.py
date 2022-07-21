7259f68f512845da9e891faf6e0d0d0393d078dd
ario <cybera.3s@gmail.com>
2022-07-21 14:36:43 +0430

    refactor test_get_request_root_should_succeed

--- Cafe_project/app/landing/tests/test_views.py
+++ Cafe_project/app/landing/tests/test_views.py
@@ -3,5 +3,5 @@ from .conftest import *
 
 def test_get_request_root_should_succeed(client):
     response = client.get("/")
-    print(response)
+    assert "<title>Home</title>" in response.get_data(as_text=True)
     assert response.status_code == 200
