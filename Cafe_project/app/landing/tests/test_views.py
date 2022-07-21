a5b1e9ae7d82dea67dd0f599a7bb4ab3975b2116
ario <cybera.3s@gmail.com>
2022-07-21 15:00:24 +0430

    add test_post_request_index_should_fail

--- Cafe_project/app/landing/tests/test_views.py
+++ Cafe_project/app/landing/tests/test_views.py
@@ -7,3 +7,9 @@ def test_get_request_index_should_succeed(client):
     response = client.get(url)
     assert b"<title>Home</title>" in response.data
     assert response.status_code == 200
+
+
+def test_post_request_index_should_fail(client):
+    url = url_for('landing.index')
+    response = client.post(url)
+    assert response.status_code == 405
