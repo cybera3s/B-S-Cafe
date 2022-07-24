ea43a2d4b7652a2b4edd37cb7cc1efc99a2cffad
ario <cybera.3s@gmail.com>
2022-07-24 13:25:16 +0430

    refactor run.py

--- Cafe_project/run.py
+++ Cafe_project/run.py
@@ -1,5 +1,8 @@
 from app import create_app
+import os
 
+env = os.environ.get('WEBAPP_ENV', 'dev')
+app = create_app(f'config.{env.capitalize()}Config')
 
-app = create_app()
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
