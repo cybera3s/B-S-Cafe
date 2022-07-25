8c05d29e521d94eb83dfe75fe5f9835e0913d55c
ario <cybera.3s@gmail.com>
2022-07-25 18:20:08 +0430

    add config file

--- Cafe_project/config.py
+++ Cafe_project/config.py
@@ -16,27 +16,27 @@ class ProdConfig(Config):
 class DevConfig(Config):
     # Statement for enabling the development environment
     DEBUG = True
-    # Define the database - we are working with -> SQLite
+    # Define the data - we are working with -> SQLite
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'cafe.db')
     DATABASE_CONNECT_OPTIONS = {}
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # upload config
-    UPLOAD_FOLDER = "static/images/menu_items"
+    UPLOAD_FOLDER = "app/static/images/menu_items"
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
     MAX_CONTENT_LENGTH = 5 * 1000 * 1000  # set maximum volume to 5 MB for a uploaded content
     THREADS_PER_PAGE = 2
     CSRF_ENABLED = True
-
+    SQLALCHEMY_ECHO = False
     CSRF_SESSION_KEY = urandom(24)
 
     # Secret key for signing cookies
     SECRET_KEY = urandom(24)
 
 
-class TestConfig(Config):
-    DEBUG = True
-    DEBUG_TB_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CACHE_TYPE = 'null'
-    WTF_CSRF_ENABLED = False
+# class TestConfig(Config):
+#     DEBUG = True
+#     DEBUG_TB_ENABLED = False
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     CACHE_TYPE = 'null'
+#     WTF_CSRF_ENABLED = False
