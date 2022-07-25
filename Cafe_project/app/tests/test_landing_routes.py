1775467c215c98a17d04353566f760cf32a9af85
ario <cybera.3s@gmail.com>
2022-07-25 19:55:40 +0430

    refactor test_landing_routes.py

--- Cafe_project/app/tests/test_landing_routes.py
+++ Cafe_project/app/tests/test_landing_routes.py
@@ -1,18 +1,34 @@
-from .conftest import *
 from flask import url_for
+from unittest import TestCase
+from app import create_app
+from app.database import db
 
 
-def test_(client, app):
-    res = client.get('/')
-    print(res)
+class TestURLs(TestCase):
+    def setUp(self):
+        app = create_app('config.TestConfig')
+        self.client = app.test_client()
+        db.app = app
+        db.create_all()
 
+    def test_get_request_index_should_have_succeed(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"<title>Home</title>", response.data)
+
+    def test_post_request_index_should_fail(self):
+        response = self.client.post('/')
+        self.assertEqual(response.status_code, 405)
+
+    def tearDown(self):
+        db.session.remove()
 
 # def test_get_request_index_should_succeed(client):
 #     url = url_for('landing.index')
 #     response = client.get(url)
 #     assert b"<title>Home</title>" in response.data
 #     assert response.status_code == 200
-
+#
 #
 # def test_post_request_index_should_fail(client):
 #     url = url_for('landing.index')
