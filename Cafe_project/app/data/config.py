ba5a1c6eb3dc072f9382f6106a5a0c38f1b2c986
ario <cybera.3s@gmail.com>
2022-07-25 18:29:56 +0430

    del extra files

--- Cafe_project/app/data/config.py
+++
@@ -1,7 +0,0 @@
-dbConfig = {
-    "DEFAULT_HOST": "localhost",
-    "DEFAULT_USER": "cafe",
-    "DEFAULT_PASSWORD": "Cafe123456789",
-    "DEFAULT_DATABASE_NAME": "cafe",
-    "DEFAULT_PORT": 5432,
-}
