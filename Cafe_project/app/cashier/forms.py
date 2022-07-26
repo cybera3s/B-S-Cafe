a4c589cc16a99af09061f8da7a287cd29c31b88f
ario <cybera.3s@gmail.com>
2022-07-27 00:26:09 +0430

    add AboutSettingForm

--- Cafe_project/app/cashier/forms.py
+++ Cafe_project/app/cashier/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, TextAreaField, URLField
 from wtforms.validators import DataRequired, NumberRange
 
 
@@ -10,3 +10,14 @@ class AddNewTableForm(FlaskForm):
                                         NumberRange(min=2, max=20, message="Capacity must be between 2 and 20")],
                             render_kw={"placeholder": "Capacity here"}
                             )
+
+
+class AboutSettingForm(FlaskForm):
+    paragraph1 = TextAreaField('Paragraph 1', render_kw={"placeholder": "Paragraph 1 here"})
+    paragraph2 = TextAreaField('Paragraph 2', render_kw={"placeholder": "Paragraph 2 here"})
+    paragraph3 = TextAreaField('Paragraph 3', render_kw={"placeholder": "Paragraph 3 here"})
+    manager1 = StringField('Manager 1', render_kw={"placeholder": "Manager 1 here"})
+    manager2 = StringField('Manager 2', render_kw={"placeholder": "Manager 2 here"})
+    manager3 = StringField('Manager 3', render_kw={"placeholder": "Manager 3 here"})
+    manager4 = StringField('Manager 4', render_kw={"placeholder": "Manager 4 here"})
+    banner_url = URLField('Banner Url', validators=[DataRequired()], render_kw={"placeholder": "Banner Url here"})
