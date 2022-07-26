5efe83bd46851d443060a8a62b24352084338e10
ario <cybera.3s@gmail.com>
2022-07-26 13:10:03 +0430

    refactor unittest/test_landing_routes

--- Cafe_project/app/tests/test_landing_routes.py
+++ Cafe_project/app/tests/test_landing_routes.py
@@ -2,14 +2,21 @@ from flask import url_for
 from unittest import TestCase
 from app import create_app
 from app.database import db
+from app.models import *
 
 
-class TestURLs(TestCase):
+class BaseTest(TestCase):
     def setUp(self):
         app = create_app('config.TestConfig')
         self.client = app.test_client()
         db.app = app
         db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+
+
+class TestRoutes(BaseTest):
 
     def test_get_request_index_should_have_succeed(self):
         response = self.client.get('/')
@@ -20,27 +27,40 @@ class TestURLs(TestCase):
         response = self.client.post('/')
         self.assertEqual(response.status_code, 405)
 
-    def tearDown(self):
-        db.session.remove()
+    def test_get_request_home_should_succeed(self):
+        response = self.client.get('/home')
+        self.assertEqual(response.status_code, 200)
 
-# def test_get_request_index_should_succeed(client):
-#     url = url_for('landing.index')
-#     response = client.get(url)
-#     assert b"<title>Home</title>" in response.data
-#     assert response.status_code == 200
-#
-#
-# def test_post_request_index_should_fail(client):
-#     url = url_for('landing.index')
-#     response = client.post(url)
-#     assert response.status_code == 405
-#
-#
-# def test_get_request_index_should_have_table_context(client, captured_templates):
-#     url = url_for('landing.index')
-#     response = client.get(url)
-#
-#     template, context = captured_templates[0]
-#
-#     assert 'tables' in context
-#     assert template.name == "landing/index.html"
+    def test_get_request_menu_should_succeed(self):
+        response = self.client.get('/menu')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_request_order_non_existing_table_should_fail(self):
+        response = self.client.get('/order/1')
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_request_order_existing_table_should_succeed(self):
+        new_table = Table(capacity=4, position='any')
+        new_table.create()
+        response = self.client.get('/order/' + str(new_table.id))
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_request_order_should_set_cookies(self):
+        new_table = Table(capacity=4, position='any')
+        new_table.create()
+        response = self.client.get('/order/' + str(new_table.id))
+
+        cookies_list = list(filter(lambda i: i[0] == 'Set-Cookie', response.headers))
+
+        self.assertIn(('Set-Cookie', f'table_id={new_table.id}; Path=/'), cookies_list)
+        self.assertIn(('Set-Cookie', 'receipt=pending; Path=/'), cookies_list)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_request_order_busy_table_should_fail(self):
+        new_table = Table(capacity=4, position='any', status=True)
+        new_table.create()
+        response = self.client.get('/order/' + str(new_table.id))
+
+        self.assertEqual(response.data, b'Table is Busy')
+        self.assertEqual(response.status_code, 400)
+
