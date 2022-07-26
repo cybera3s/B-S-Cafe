f11adea9f4dbf6151037a1317e7de1bfd6b08a84
ario <cybera.3s@gmail.com>
2022-07-26 13:25:20 +0430

    add test_get_request_index_should_have_table_context

--- Cafe_project/app/tests/test_landing_controllers.py
+++ Cafe_project/app/tests/test_landing_controllers.py
@@ -1,31 +1,20 @@
-from .conftest import test_client as client, init_database as init_db
+from .conftest import test_client as client, init_database as init_db, captured_templates
 from app.models import *
 from flask import url_for
 import json
 import pytest
 
 
-# def test_get_request_index_should_succeed(client):
-#     url = url_for('landing.index')
-#     response = client.get(url)
-#     assert b"<title>Home</title>" in response.data
-#     assert response.status_code == 200
-#
-#
-# def test_post_request_index_should_fail(client):
-#     url = url_for('landing.index')
-#     response = client.post(url)
-#     assert response.status_code == 405
-#
-#
-# def test_get_request_index_should_have_table_context(client, captured_templates):
-#     url = url_for('landing.index')
-#     response = client.get(url)
-#
-#     template, context = captured_templates[0]
-#
-#     assert 'tables' in context
-#     assert template.name == "landing/index.html"
+@pytest.mark.skip
+def test_get_request_index_should_have_table_context(client, captured_templates):
+    url = url_for('landing.index')
+    response = client.get(url)
+
+    template, context = captured_templates[0]
+
+    assert 'tables' in context
+    assert template.name == "landing/index.html"
+
 
 def test_get_request_index_should_have_succeed(client, init_db):
     """
