a8debfd31fcf68ec83795b822178266a1da89581
ario <cybera.3s@gmail.com>
2022-07-26 18:48:08 +0430

    add migrate

--- Cafe_project/manage.py
+++ Cafe_project/manage.py
@@ -1,9 +1,11 @@
 from run import app
 from app.database import db
 from app.models import *
+from app import migrate
 
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(app=app, db=db, cashier=Cashier, table=Table, receipt=Receipt, order=Order, status=Status,
-                menuitem=MenuItem, discount=Discount, category=Category)
+                menuitem=MenuItem, discount=Discount, category=Category, migrate=migrate
+                )
