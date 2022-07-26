f559823d7b6a25cfe32302b2eaebb6a2d58769b7
ario <cybera.3s@gmail.com>
2022-07-26 12:54:54 +0430

    refactor manage.py

--- Cafe_project/manage.py
+++ Cafe_project/manage.py
@@ -6,4 +6,4 @@ from app.models import *
 @app.shell_context_processor
 def make_shell_context():
     return dict(app=app, db=db, cashier=Cashier, table=Table, receipt=Receipt, order=Order, status=Status,
-                manuitem=MenuItem, discount=Discount, category=Category)
+                menuitem=MenuItem, discount=Discount, category=Category)
