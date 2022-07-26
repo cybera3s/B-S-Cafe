3e00b1727664b730fc137fa5fa72c9f0ef8060de
ario <cybera.3s@gmail.com>
2022-07-26 23:38:32 +0430

    add about_setting

--- Cafe_project/manage.py
+++ Cafe_project/manage.py
@@ -2,10 +2,12 @@ from run import app
 from app.database import db
 from app.models import *
 from app import migrate
+from app.cashier.models import *
 
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(app=app, db=db, cashier=Cashier, table=Table, receipt=Receipt, order=Order, status=Status,
-                menuitem=MenuItem, discount=Discount, category=Category, migrate=migrate
+                menuitem=MenuItem, discount=Discount, category=Category, migrate=migrate,
+                about_setting=AboutSetting
                 )
