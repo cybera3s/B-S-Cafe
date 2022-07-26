ab566b1a023f74fd47944c58026d4854191b0987
ario <cybera.3s@gmail.com>
2022-07-26 23:37:59 +0430

    add AboutSetting model

---
+++ Cafe_project/app/cashier/models.py
@@ -0,0 +1,15 @@
+from app.database import db
+from app.models import BaseModel
+
+
+class AboutSetting(BaseModel):
+    paragraph1 = db.Column(db.String(264), nullable=True)
+    paragraph2 = db.Column(db.String(264), nullable=True)
+    paragraph3 = db.Column(db.String(264), nullable=True)
+
+    manager1 = db.Column(db.String(100), nullable=True)
+    manager2 = db.Column(db.String(100), nullable=True)
+    manager3 = db.Column(db.String(100), nullable=True)
+    manager4 = db.Column(db.String(100), nullable=True)
+
+    banner_url = db.Column(db.String(300), nullable=False)
