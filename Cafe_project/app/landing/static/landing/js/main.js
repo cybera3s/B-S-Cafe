b4b21336ff383049ca6262a8768430423eec164c
ario <cybera.3s@gmail.com>
2022-07-28 00:22:27 +0430

    refactor sendContactUsForm func

--- Cafe_project/app/landing/static/landing/js/main.js
+++ Cafe_project/app/landing/static/landing/js/main.js
@@ -227,6 +227,7 @@ $(document).ready(function () {
             },
             error: function (err) {
                 console.log(err);
+                swal("Failed", err, "error");
             }
         });
     };
