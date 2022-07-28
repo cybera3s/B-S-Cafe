c069c2132bf470ceb3bd53ef20bbcce4f9ad1592
ario <cybera.3s@gmail.com>
2022-07-28 20:00:39 +0430

    add CashierProfile form

--- Cafe_project/app/cashier/forms.py
+++ Cafe_project/app/cashier/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField, URLField, EmailField, PasswordField
 from wtforms.validators import DataRequired, NumberRange, Length
+from wtforms.widgets import HiddenInput
+
 from app.models import Cashier
 
 
@@ -26,9 +28,6 @@ class LoginForm(FlaskForm):
         return True
 
 
-
-
-
 class AddNewTableForm(FlaskForm):
     position = StringField('Position', validators=[DataRequired()], render_kw={"placeholder": "Position here"})
     capacity = IntegerField('Capacity',
@@ -47,3 +46,40 @@ class AboutSettingForm(FlaskForm):
     manager3 = StringField('Manager 3', render_kw={"placeholder": "Manager 3 here"})
     manager4 = StringField('Manager 4', render_kw={"placeholder": "Manager 4 here"})
     banner_url = URLField('Banner Url', validators=[DataRequired()], render_kw={"placeholder": "Banner Url here"})
+
+
+class CashierProfile(FlaskForm):
+    id = IntegerField(widget=HiddenInput())
+    first_name = StringField('First Name', validators=[DataRequired(), Length(max=150)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(max=150)])
+    email = EmailField('Email', validators=[DataRequired(), Length(max=150)])
+    phone_number = StringField('Phone Number', validators=[DataRequired(), Length(max=150)])
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired(), NumberRange(min=4, max=100, message="Password must be between 4 and 100")]
+    )
+
+    def validate(self):
+        check_validate = super(CashierProfile, self).validate()
+        # if our validators do not pass
+        if not check_validate:
+            return False
+
+        # get cashier
+        cashier = Cashier.query.get(self.id.data)
+        if self.email.data != cashier.email:
+            # check exist of email for uniqueness
+            c = Cashier.query.filter_by(email=self.email.data).first()
+            if c:
+                self.form_errors.append("Duplicate Email Address!")
+                return False
+        if self.phone_number.data != cashier.phone_number:
+            # check exist of email for uniqueness
+            c = Cashier.query.filter_by(email=self.phone_number.data).first()
+            if c:
+                self.id.errors.append("Duplicate Phone Number!")
+                return False
+        # hash password
+        cashier.set_password(self.password.data)
+
+        return True
