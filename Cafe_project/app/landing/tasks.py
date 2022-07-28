f4938ecf34bf4273320f00360094ee24e09bce0a
ario <cybera.3s@gmail.com>
2022-07-28 13:32:49 +0430

    refactor send_email task

--- Cafe_project/app/landing/tasks.py
+++ Cafe_project/app/landing/tasks.py
@@ -1,17 +1,18 @@
 from flask_mail import Message
 from app.extensions import mail
-from celery import current_app as celery
+from celery_runner import celery
 
 
-@celery.task(bind=True, time_limit=60, rate_limit='20/m', max_retries=10, default_retry_delay=30)
+@celery.task(bind=True, time_limit=60, rate_limit='20/m', max_retries=10, default_retry_delay=30,
+             ignore_result=True)
 def send_email(self, data):
     try:
         msg = Message(subject="Contact Us Feedback", recipients=['cybera.3s@gmail.com'])
 
         msg.html = f"""<h1>From: {data['email']}</h1>
-        <h1>First Name: {data['first_name']}</h1>
-        <h1>Last Name: {data['last_name']}</h1>
-        </h1><p>Feedback: {data['feedback']}</p>"""
+            <h1>First Name: {data['first_name']}</h1>
+            <h1>Last Name: {data['last_name']}</h1>
+            </h1><p>Feedback: {data['feedback']}</p>"""
 
         mail.send(msg)
         return "Email Sent"
