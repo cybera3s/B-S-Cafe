39e295a80402b8b706ae2d0a30b8a5b30f0f462a
ario <cybera.3s@gmail.com>
2022-07-29 00:31:55 +0430

    add inject_status context processor

--- Cafe_project/app/cashier/__init__.py
+++ Cafe_project/app/cashier/__init__.py
@@ -1,4 +1,9 @@
-
 def create_module(app, **kwargs):
     from .routes import cashier
+    from app.models import Status
+
+    @cashier.context_processor
+    def inject_status():
+        return dict(all_status=Status.query.all())
+
     app.register_blueprint(cashier)
