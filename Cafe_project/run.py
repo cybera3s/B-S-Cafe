6ba6bb4f67a417b79ff3790fc475f9d81eba0d27
ario <cybera.3s@gmail.com>
2022-07-28 15:56:46 +0430

    add register commands

--- Cafe_project/run.py
+++ Cafe_project/run.py
@@ -1,8 +1,11 @@
 from app import create_app
 import os
+from app.cli import register
 
 env = os.environ.get('WEBAPP_ENV', 'dev')
 app = create_app(f'config.{env.capitalize()}Config')
+
+register(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
