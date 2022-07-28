b4c17fc18a780e80bbb01f01dd91be2d1a55351f
ario <cybera.3s@gmail.com>
2022-07-28 22:28:35 +0430

    add changeStatus func

--- Cafe_project/app/cashier/static/js/cashier/orders/orders.js
+++ Cafe_project/app/cashier/static/js/cashier/orders/orders.js
@@ -26,6 +26,38 @@ $(document).ready(function () {
         });
     }
 
+    function changeStatus(event, url="http://127.0.0.1:5000/admin/cashier_panel/order") {
+        /*
+            Send PUT request to change status of a order
+        */
+        $(this).parents('.dropdown').find('button').text($(this).text());
+
+        let Data = {
+            orderId: +$(this).data('order'),
+            statusId: +$(this).attr('id'),
+        };
+
+        $.ajax({
+            url: url,
+            type: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(Data),
+            success: function (response) {
+                swal("Successful", "Order Status Changed!", "success", {
+                    buttons: false,
+                    timer: 2000,
+                });
+            },
+            error: function (err) {
+                console.log(err);
+                 swal("Failed", "Something Went Wrong on changing status!", "error");
+            }
+        });
+    };
+
+    // click event for dropdown to change order status
+    $(".content").on("click", ".dropdown-menu a", changeStatus);
+
     // click event for each row of table
     $('.data').on('click', getReceiptDetail)
 });
