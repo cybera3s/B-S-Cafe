93dc335ce45ec329956b6946f3091a418c49df71
ario <cybera.3s@gmail.com>
2022-07-30 20:18:13 +0430

    refactor menu_item_index.js

--- Cafe_project/app/cashier/static/js/cashier/menu_items/menu_item_index.js
+++ Cafe_project/app/cashier/static/js/cashier/menu_items/menu_item_index.js
@@ -55,4 +55,78 @@ $(document).ready(function () {
 
     // click event to remove menu item
     $('.delete-item').on('click', removeMenuItem);
+
+
+    function showUploadedImage() {
+        /*
+            validate and show uploaded image
+        */
+        let file = $(this).prop('files')[0];
+
+        // image type validation
+        if (!/image\/*/.test(file.type)) {
+            swal(
+                'Failed',
+                'Wrong type for image input!',
+                'error'
+            );
+            $(this).focus();
+            return;
+        };
+
+        let tempURL = URL.createObjectURL(file);
+
+        $("#targetImage").attr('src', tempURL);
+        $("label[for='targetImage']").text(file.name);
+
+    };
+
+    function sendEditMenuItemForm(e, url = url_root) {
+        /*
+            Send form fields data with POST request
+        */
+        e.preventDefault();
+
+        url += listMenuItemUrl; // send form data here.
+        let csrfToken = $(this).children('#csrf_token').val();  // get contact us form csrf token
+
+        $.ajax({
+            // Inject our CSRF token into our AJAX request.
+            beforeSend: function (xhr, settings) {
+                if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrfToken);
+                }
+            },
+            type: "POST",
+            url: url,
+            data: new FormData(this), // serializes the form's elements.
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                swal("Successful", data, "success")
+                    .then(result => {
+                        window.location.reload();   // reload page on click ok alert
+                    });
+            },
+            error: function (err) {
+                let errors = err.responseJSON;
+
+                swal("Failed", "Please Correct Errors!", "error");
+
+                for (let field in errors) {
+                    let f = $("#editMenuItemForm").find(`#${field}`);
+                    f.addClass('is-invalid').next().text(errors[field].join(' '))
+                }
+
+            }
+        });
+    };
+
+    // show uploaded image in img tag on change of file input
+    $('.content').on('change', "#image", showUploadedImage);
+
+    // submit event of edit menu item
+    $(".content").on("submit", "#editMenuItemForm", sendEditMenuItemForm);
+
 })
