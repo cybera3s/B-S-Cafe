8b648d84594789243cb5fdfe00491d4c31a74d75
ario <cybera.3s@gmail.com>
2022-07-31 19:10:52 +0430

    add getCategoryModalForm function

--- Cafe_project/app/cashier/static/js/cashier/category/category_index.js
+++ Cafe_project/app/cashier/static/js/cashier/category/category_index.js
@@ -14,17 +14,41 @@ $(document).ready(function () {
         $.ajax({
             url: url,
             method: 'DELETE',
-            success: function (data){
+            success: function (data) {
                 swal('Done', data, 'success');
                 $(row).remove();
             },
-            error: function (error){
+            error: function (error) {
                 console.log(error);
                 swal('Failed', data, 'error');
             }
         });
 
     };
+
+    function getCategoryModalForm(e, url = url_root) {
+        /*
+            Send GET request to fill out edit category modal
+        */
+        e.preventDefault();
+        let row = $(this).parents('tr');
+        let catId = row.attr('id');
+
+        url += BASE_URL + `/${catId}`;
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+            success: function (data) {
+                $(".modal-body").empty();
+                $(".modal-body").append(data);
+            },
+            error: function (error) {
+                console.log(error);
+                swal('Failed', 'Something went Wrong!' + error, 'error');
+            }
+        });
+    }
 
 
     // remove category btn click event
