d17388a6153087a18fa84fcf6191f91a88f785a8
ario <cybera.3s@gmail.com>
2022-07-31 19:12:57 +0430

    add submitModifyCategory function

--- Cafe_project/app/cashier/static/js/cashier/category/category_index.js
+++ Cafe_project/app/cashier/static/js/cashier/category/category_index.js
@@ -50,6 +50,46 @@ $(document).ready(function () {
         });
     }
 
+    function submitModifyCategory(event, url = url_root){
+        /*
+            Send PUT request to update a category
+        */
+        event.preventDefault();
+
+        let catId = $(this).children('#id').val()
+        url += BASE_URL + `/${catId}` ;
+        let csrfToken = $(this).children('#csrf_token').val();  // get form csrf token
+
+        $.ajax({
+            // Inject our CSRF token into our AJAX request.
+            beforeSend: function (xhr, settings) {
+                if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrfToken);
+                }
+            },
+            type: "PUT",
+            url: url,
+            data: $(this).serialize(), // serializes the form's elements.
+            success: function (data) {
+                swal("Successful", data, "success")
+                    .then(result => {
+                        window.location.reload();   // reload page on click ok alert
+                    });
+            },
+            error: function (err) {
+                console.log(err);
+                let errors = err.responseJSON;
+                swal("Failed", "Please Correct Errors!", "error");
+                // bind errors with their fields
+                for (let field in errors) {
+                    let f = $("form").find(`#${field}`);
+                    f.addClass('is-invalid').next().text(errors[field].join(' '))
+                }
+
+            }
+        });
+
+    }
 
     // remove category btn click event
     $('tr .delete-btn').click(deleteCategory);
