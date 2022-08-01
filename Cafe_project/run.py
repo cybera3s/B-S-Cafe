60556cd21caf6c965c7e28b6617a396b5d2c10c1
ario <cybera.3s@gmail.com>
2022-08-01 15:03:47 +0430

    check for email and celery config on start

--- Cafe_project/run.py
+++ Cafe_project/run.py
@@ -1,9 +1,19 @@
-from app import create_app
+import sys
 import os
+
+from app import create_app
 from app.cli import register
+from app.utils.utils import check_email_configuration
 
 env = os.environ.get('WEBAPP_ENV', 'dev')
 app = create_app(f'config.{env.capitalize()}Config')
+
+# check email and celery configuration
+try:
+    check_email_configuration(app)
+except Exception as e:
+    print(str(e))
+    sys.exit()
 
 register(app)
 
